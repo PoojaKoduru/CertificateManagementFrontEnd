5d660c09f5c0da1462e5ea83f7b5d35cc27af545
PoojaKoduru <66681542+PoojaKoduru@users.noreply.github.com>
2022-09-15 23:55:32 +0530

    Delete my-react-app directory

--- my-react-app/src/SecondComponent.js
+++
@@ -1,9 +0,0 @@
-function SecondComponent(){
-    return(
-        <div className="SecondComponent">
-            <h3>This is a practice component</h3>
-        </div>
-    )
-}
-
-export default SecondComponent;
