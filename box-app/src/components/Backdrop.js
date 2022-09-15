fa227dda939d04b777c8ebbf7f21f66e64ce98b8
PoojaKoduru <66681542+PoojaKoduru@users.noreply.github.com>
2022-09-15 23:54:38 +0530

    Delete box-app directory

--- box-app/src/components/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import styles from './Backdrop.module.css'
-function Backdrop(props){
-    return(
-        <div className={styles.backdrop} onClick={props.onCancel}></div>
-    )
-}
-
-export default Backdrop
